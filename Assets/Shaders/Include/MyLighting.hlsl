1f1abd1b3df3411e5471bb9aa78c22054435b354
Alexandra Petrova <alex.petrova@email.cz>
2021-03-20 17:46:34 +0100

    Toon Shader update
    Updated the toon shader, added multiple light sources support (witout light color support though).

--- Assets/Shaders/Include/MyLighting.hlsl
+++ Assets/Shaders/Include/MyLighting.hlsl
@@ -33,7 +33,8 @@ void GetMyLight_half(float3 WorldPos, out float3 Direction, out float3 Color, ou
 	#endif
 }
 
-void GetMySpecular_half(half3 Specular, half Smoothness, half3 Direction, half3 Color, half3 WorldNormal, half3 WorldView, out half3 Out) {
+void GetMySpecular_half(half3 Specular, half Smoothness, half3 Direction, half3 Color, half3 WorldNormal, 
+	half3 WorldView, out half3 Out) {
 	#ifdef SHADERGRAPH_PREVIEW
 		Out = 0;
 	#else
@@ -42,4 +43,29 @@ void GetMySpecular_half(half3 Specular, half Smoothness, half3 Direction, half3 
 		WorldView = normalize(WorldView);
 		Out = LightingSpecular(Color, Direction, WorldNormal, WorldView, half4(Specular, 0), Smoothness);
 	#endif
+}
+
+void GetMyAdditionalLights_half(half3 SpecularColor, half Smoothness, half3 WorldPosition, half3 WorldNormal, half3 WorldView, 
+	out half3 Diffuse, out half3 Specular)
+{
+	half3 diffuseColor = 0;
+	half3 specularColor = 0;
+
+	#ifndef SHADERGRAPH_PREVIEW
+		Smoothness = exp2(10 * Smoothness + 1);
+		WorldNormal = normalize(WorldNormal);
+		WorldView = SafeNormalize(WorldView);
+		int pixelLightCount = GetAdditionalLightsCount();
+		for (int i = 0; i < pixelLightCount; ++i)
+		{
+			Light light = GetAdditionalLight(i, WorldPosition);
+			half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+			diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
+			specularColor += LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, 
+				half4(SpecularColor, 0), Smoothness);
+		}
+	#endif
+
+	Diffuse = diffuseColor;
+	Specular = specularColor;
 }
