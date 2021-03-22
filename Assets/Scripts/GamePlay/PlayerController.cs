20faa77add26681f037982a81db6dca4ae714d97
Rinter-7 <rinter77@gmail.com>
2021-03-22 12:37:53 +0100

    Character controller + jumping

--- Assets/Scripts/GamePlay/PlayerController.cs
+++ Assets/Scripts/GamePlay/PlayerController.cs
@@ -1,41 +1,95 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour, IDamagableObject
 {
+    public CharacterController characterController;
     public GameObject bulletPrefab;
     public float speed = 3;
     public float health = 20;
 
+    public float jumpHeight = 1.0f;
+
+    private const float gravity = -9.81f;
+    private float upVelocity = 0;
+
+    private Camera mainCamera;
+    private Transform cameraTrans;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        cameraTrans = mainCamera.transform;
+    }
     // Update is called once per frame
     void Update()
     {
-        Vector3 velocity = Vector3.zero;
-        float delta = Time.deltaTime;
-        if (Input.GetKey(KeyCode.D))
-            velocity += new Vector3(1, 0, 0);
-        if (Input.GetKey(KeyCode.A))
-            velocity += new Vector3(-1, 0, 0);
+        MoveUpdate();
 
-        if (Input.GetKey(KeyCode.W))
-            velocity += new Vector3(0, 0, 1);
-        if (Input.GetKey(KeyCode.S))
-            velocity += new Vector3(0, 0, -1);
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-        if (velocity.magnitude > 0)
-            transform.position += (velocity.normalized) * delta * speed;
-
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-            Shoot();
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out RaycastHit hit, 1000))
+        RaycastHit hit;
+        bool didHit = false;
+        if (didHit = Physics.Raycast(ray, out hit, 1000))
         {
             var targetPosition = hit.point;
             targetPosition.y = transform.position.y;
             transform.LookAt(targetPosition);
         }
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            if (didHit)
+                ShootPrecise(hit);
+            else
+                Shoot();
+    }
+
+
+
+    void MoveUpdate()
+    {
+        Vector3 forwardV = cameraTrans.forward;
+        forwardV.y = 0;
+        forwardV.Normalize();
+        Vector3 rightV = cameraTrans.right;
+        Vector3 velocity = Vector3.zero;
+        float delta = Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+            velocity += rightV;
+        if (Input.GetKey(KeyCode.A))
+            velocity -= rightV;
+
+        if (Input.GetKey(KeyCode.W))
+            velocity += forwardV;
+        if (Input.GetKey(KeyCode.S))
+            velocity -= forwardV;
+
+        velocity.Normalize();
+        velocity *= speed;
+
+        if (characterController.isGrounded)
+        {
+            upVelocity = 0;
+            if (Input.GetKey(KeyCode.Space))
+                upVelocity += Mathf.Sqrt(jumpHeight * -3f * gravity);           
+        }
+        else
+            upVelocity += gravity * delta;
+
+        velocity.y = upVelocity;
+        Debug.Log("Velocity is" + velocity);
+
+        if (velocity.magnitude > 0)
+            characterController.Move(velocity * delta);
+    }
+
+    private void ShootPrecise(RaycastHit hit)
+    {
+        var bulletInstanceTrans = Instantiate(bulletPrefab).transform;
+        bulletInstanceTrans.position = transform.position;
+        bulletInstanceTrans.rotation = Quaternion.LookRotation( hit.point - transform.position );
     }
 
     void Shoot()
